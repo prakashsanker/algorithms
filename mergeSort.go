07fb3c5d7e258de6b3b3827c137d1c529f543f9e
psanker <psanker@splunk.com>
2015-05-19 16:33:59 -0700

    merge method

---
+++ mergeSort.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+
+
+}
+
+
+func merge (slice1 []int, index1 int, index2, int, index3) []int {
+	subarr1 = make([]int, 5,5);
+	subarr2 = make([]int, 5, 5);
+	j := 0;
+	for i := index1; i <= index2; i++ {
+		subarr1[j] = slice1[index1]
+		j++
+	}
+
+	j = 0
+	for i := index2; i <= index2; i++ {
+		subarr1[j] = slice2[i]
+		j++
+	}
+	j = 0
+	k = 0
+
+	for i := index1; i <= index3; i++ {
+		
+	}
+
+
+
+}
